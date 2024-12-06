c3eaa0e62729f72a6ce5238cdf7dd245270bea46
jjbel <jaibellare@gmail.com>
2024-12-06 19:52:02 +0530

    start routing: add rockets page

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,12 @@
 import styles from "./page.module.css";
+import Link from "next/link";
 
-export default function Home(){
-    return (
-        <div className={styles.page}>
-            Hello!
-        </div>
-    )
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      Homepage
+      <br />
+      <Link href="/rockets">Rockets</Link>
+    </div>
+  );
 }
