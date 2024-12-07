56920f6996bfd1c87d7bde2aa2435374b3f5f7aa
jjbel <jaibellare@gmail.com>
2024-12-07 10:47:20 +0530

    configure next as static, add gh action

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+// from https://github.com/gregrickaby/nextjs-github-pages
+
+const nextConfig = {
+  /**
+   * Enable static exports for the App Router.
+   * https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+   */
+  output: "export",
+
+  /**
+   * Set base path. This is the slug of your GitHub repository.
+   * https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+   */
+  basePath: "/nextjs-testing",
+
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   * https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
