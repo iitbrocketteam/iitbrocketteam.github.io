0825157b285aaaa41013d0a1e7dcd96d314dbce1
jjbel <jaibellare@gmail.com>
2024-12-30 00:12:50 +0530

    add contact page

---
+++ app/contact/page.jsx
@@ -0,0 +1,41 @@
+import styles from "./contact.module.css";
+
+// TODO use https://www.npmjs.com/package/react-modal
+// this https://www.npmjs.com/package/reactjs-popup gives uses client error
+
+export default function Contact() {
+  return (
+    <div className={styles.page}>
+      <h1 className={styles.heading}>Contact Us!</h1>
+
+      <form className={styles.form}>
+        <input
+          placeholder="Organization"
+          className={styles.input}
+          type="text"
+          id="org"
+          name="org"
+        />
+
+        <input
+          placeholder="Email"
+          className={styles.input}
+          type="email"
+          id="email"
+          name="email"
+        />
+
+        <input
+          placeholder="Phone"
+          className={styles.input}
+          type="tel"
+          id="phone"
+          name="phone"
+          pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"
+        />
+
+        <input className={styles.submit} type="submit" value="SUBMIT" />
+      </form>
+    </div>
+  );
+}
