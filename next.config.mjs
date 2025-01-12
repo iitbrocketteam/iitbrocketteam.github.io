0ee30a2424f020315113cbb35cc60cc3af555e47
Jai Bellare <jaibellare@gmail.com>
2025-01-12 17:36:42 +0530

    next: set basePath for github pages

--- next.config.mjs
+++ next.config.mjs
@@ -14,7 +14,7 @@ const nextConfig = {
    * https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
   //   only for github pages
-  //   basePath: "/nextjs-testing",
+  basePath: "/nextjs-testing",
 
   /**
    * Disable server-based image optimization. Next.js does not support
