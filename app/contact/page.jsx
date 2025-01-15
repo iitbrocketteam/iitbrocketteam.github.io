6ea356cd4abe2dc48cb17c8fcc048fa1ababde7a
Jai Bellare <jaibellare@gmail.com>
2025-01-15 19:21:56 +0530

    contact: improve styling

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,14 +1,21 @@
 import styles from "./contact.module.css";
 
+import { Roboto } from "next/font/google";
+
+const font = Roboto({
+  weight: ["300", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
 // TODO use https://www.npmjs.com/package/react-modal
 // this https://www.npmjs.com/package/reactjs-popup gives uses client error
 
 export default function Contact() {
   return (
-    <div className={styles.page}>
-      <h1 className={styles.heading}>Contact Us!</h1>
-
+    <div className={styles.page + ' ' + font.className}>
       <form className={styles.form}>
+        <h1 className={styles.heading}>Contact Us</h1>
         <input
           placeholder="Organization"
           className={styles.input}
@@ -43,7 +50,7 @@ export default function Contact() {
           cols="20"
         ></textarea>
 
-        <input className={styles.submit} type="submit" value="SUBMIT" />
+        <input className={styles.submit} type="submit" value="Send Us &rarr;" />
       </form>
     </div>
   );
