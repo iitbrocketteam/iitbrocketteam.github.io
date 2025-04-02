ba682dc5fbc399aea5276eef8c0f3d6cafa575df
AyushaH2005 <hongekarayusha@gmail.com>
2025-04-02 23:56:57 +0530

    rewording

--- app/sponsors/page.jsx
+++ app/sponsors/page.jsx
@@ -109,24 +109,14 @@ export default function Sponsors() {
           <li>
             <h4>CSR and Tax Exemption</h4>
             <p>
-              Support groundbreaking scientific innovation with the IIT Bombay
-              Rocket Team! Your contributions qualify for tax rebates under the
-              Income Tax Act for scientific research. This is a unique
-              opportunity to promote cutting-edge technology, empower young
-              innovators, and make a meaningful impact on the future of space
-              exploration and scientific advancement.
+            Support innovation with the IIT Bombay Rocket Team! Your tax-deductible contribution fuels research, empowers young innovators, advances space exploration, and promotes cutting-edge technology. Join us in shaping the future of scientific discovery!
             </p>
           </li>
 
           <li>
             <h4>Branding Avenues</h4>
             <p>
-              Gain global exposure by sponsoring the IIT Bombay Rocket Team!
-              Your logo will shine on our rocket soaring 30,000 feet, featured
-              at launch events, on T-shirts, and across social media (40k+
-              followers). Connect with thousands of attendees and talented IITB
-              students for internships while supporting cutting-edge innovation
-              and space exploration
+            Sponsor the IIT Bombay Rocket Team and gain global visibility. Your logo will be displayed on our rocket, team apparel, launch events, and social media (40K+ followers). Connect with top IITB talent, support innovation, and contribute to the future of space exploration.
             </p>
           </li>
 
@@ -150,36 +140,21 @@ export default function Sponsors() {
           <li>
             <h4>Monetary Contributions </h4>
             <p>
-              We seek monetary support to cover essential expenses, including
-              competition fees, materials, travel allowances, outreach,
-              publicity, and event costs. Your contributions will enable us to
-              manage operational expenses efficiently, allowing us to focus on
-              innovation, engineering excellence, and achieving success in
-              national and international competitions.
+            We seek funding for competition fees, materials, travel, outreach, and events. Your support enables us to drive innovation, achieve engineering excellence, and compete on global platforms.
             </p>
           </li>
 
           <li>
             <h4>In-Kind Sponsorships</h4>
             <p>
-              We design and builds high-quality engineering prototypes. We
-              invite in-kind sponsorships to provide mechanical and electrical
-              components, specialized materials, tools, machinery, and other
-              essential supplies for rocket manufacturing. Your contributions
-              will directly support our projects, helping us achieve engineering
-              excellence and advance the boundaries of scientific innovation.
+            We develop high-quality engineering prototypes and welcome in-kind sponsorships for components, materials, tools, and machinery. Your support directly drives innovation and engineering excellence in rocketry.
             </p>
           </li>
 
           <li>
             <h4>Services and Mentorship</h4>
             <p>
-              We seek services such as shipping, accommodation, travel planning,
-              and packaging. Access to your technical experts for guidance would
-              be invaluable. We value mentorship, assistance in connecting with
-              stakeholders, securing purchase discounts, and collaborating on
-              joint marketing initiatives like press releases and social media
-              campaigns to maximize visibility and impact.
+            We seek support for logistics, travel, packaging, and expert guidance. Collaboration on mentorship, stakeholder connections, discounts, and joint marketing will enhance our impact and visibility.
             </p>
           </li>
         </ol>
